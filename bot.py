22eb8e20e2da60a20a77a12aca7e7caddb30617f
Julian Espada <jesp1999@mit.edu>
2023-01-16 20:23:32 -0500

    Add basic polling command

--- bot.py
+++ bot.py
@@ -1,16 +1,15 @@
-import asyncio
 import os
+from os.path import join, dirname
+
 import discord
 from discord.ext import commands
-from discord.ext.commands import Context
 
 from dotenv import load_dotenv
-from src.handler.association_handler import AssociationHandler
 
+from src.cogs import reactmatch, poll
 
-load_dotenv()
+load_dotenv(join(dirname(__file__), '.env'))
 
-association_handler: AssociationHandler = AssociationHandler()
 TOKEN: str = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
@@ -25,66 +24,34 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.listen('on_ready')
 async def on_ready():
     print('Initializing HeckBot..')
+    # load cogs
+    await reactmatch.setup(bot)
+    await poll.setup(bot)
+
+    # alert channels of bot online status
     for guild in bot.guilds:
         print(f'{bot.user} has connected to the following guild: {guild.name}(id: {guild.id})')
         if guild.id == 334491082241081347:
             channel = guild.get_channel(744611387371683962)
             await channel.send('hello, i am online')
+    await bot.change_presence(
+        status=discord.Status.online,
+        activity=discord.Game('the part :)')
+    )
 
 
-@bot.command()
-async def associate(ctx: Context, *args):
-    if len(args) != 2:
-        await ctx.send('Incorrect syntax, try \"`!associate <word> <emoji>`\"')
-    else:
-        word = args[0].lower()
-        emoji = args[1]
-        association_handler.add_association(str(ctx.guild.id), word, emoji)
-        await ctx.send(f'Successfully associated the keyword \"{word}\" with the reaction \"{emoji}\"!')
-
-
-@bot.command()
-async def dissociate(ctx: Context, *args):
-    if len(args) == 1:
-        word = args[0].lower()
-        association_handler.remove_association(str(ctx.guild.id), word)
-        await ctx.send(f'Successfully dissociated the keyword \"{word}\" from all reactions!')
-    elif len(args) == 2:
-        word = args[0].lower()
-        emoji = args[1]
-        association_handler.remove_association(str(ctx.guild.id), word, emoji)
-        await ctx.send(f'Successfully dissociated the keyword \"{word}\" with the reaction \"{emoji}\"!')
-    else:
-        await ctx.send('Incorrect syntax, try \"`!dissociate <word>`\"')
-
-
-@bot.command()
-async def disassociate(ctx: Context, *args):
-    await ctx.send('The command is \"`!dissociate`\", y\'know 😉')
-
-
-@bot.listen('on_message')
-async def on_message(message: discord.Message):
-    text = message.content.lower()
-    server = message.guild
-    if message.author.bot or (await bot.get_context(message)).valid:
-        return
-
-    associations = association_handler.get_all_associations(str(server.id))
-    for word, emojis in associations.items():
-        if word in text:
-            for emoji in emojis:
-                asyncio.get_event_loop().create_task(message.add_reaction(emoji))
-
-#welcome message
+# welcome message
 @bot.listen()
 async def on_member_join(member):
-    #essentially whitelisted servers with a designated channel; we can either give the bot permission to make a channel so we could allow that on any server, or add whitelisted servers as wanted
+    # essentially whitelisted servers with a designated channel; we can either give the bot permission to make a channel
+    # so we could allow that on any server, or add whitelisted servers as wanted
     for guild in bot.guilds:
         if guild.id == 334491082241081347:
-            #placeholder channel; we need a welcome channel in HeckBoiCrue
+            # placeholder channel; we need a welcome channel in HeckBoiCrue
             channel = guild.get_channel(744611387371683962)
-            #the actual message; edit as wanted. if we allow bot to be in more servers we will have to make a command to change the message we want to send per server
+            # the actual message; edit as wanted. if we allow bot to be in more servers we will have to make a command
+            # to change the message we want to send per server
             await channel.send(f"Welcome to HeckBoiCrue <@!{member.id}>!")
+
 
 bot.run(TOKEN)
