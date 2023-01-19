69e84261f45202c5ab3f42d8cee6892a2625ae3f
Kai H <me@kaihaskell.com>
2023-01-19 12:40:27 -0500

    Added functionality for bot to join vc

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-from src.cogs import reactmatch, poll, gif
+from src.cogs import reactmatch, poll, gif, sfx
 from src.types.constants import WELCOME_CHANNEL_ID, PRIMARY_GUILD_ID, WELCOME_MESSAGE, ADMIN_CONSOLE_CHANNEL_ID, \
     BOT_ONLINE_MESSAGE, BOT_CUSTOM_STATUS, BOT_COMMAND_PREFIX
 
@@ -33,6 +33,7 @@ async def on_ready() -> None:
     await reactmatch.setup(bot)
     await poll.setup(bot)
     await gif.setup(bot)
+    await sfx.setup(bot)
 
     # alert channels of bot online status
     for guild in bot.guilds:
--- src/cogs/sfx.py
+++ src/cogs/sfx.py
@@ -1,2 +1,40 @@
 from discord.ext import commands
 from discord.ext.commands import Bot, Context
+from discord.ui import Button
+
+
+
+class Sfx(commands.Cog):
+
+    def __init__(self, bot: Bot) -> None:
+        """
+        Constructor method
+        :param bot: Instance of the running Bot
+        """
+        self._bot: Bot = bot
+
+    @commands.command()
+    async def playsound(self, ctx: Context, *search_term_parts) -> None:
+
+        # Simple check to make sure the user is actually inside of a voice channel.
+
+        user_vc = ctx.author.voice.channel
+        if user_vc is None:
+            await ctx.send('*You aren\'t in a voice channel.*')
+            return
+    
+        # Connect to the user's voice channel
+        
+        voice_client = await user_vc.connect()
+
+        # Play the sound selected with our input.
+
+        voice_client.play()
+        return
+
+async def setup(bot: Bot) -> None:
+    """
+    Setup function for registering the sfx cog.
+    :param bot: Instance of the running Bot
+    """
+    await bot.add_cog(Sfx(bot))
