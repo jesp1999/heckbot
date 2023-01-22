990f317a82813306d2ce279e66aee99e945b360a
Julian Espada <jesp1999@mit.edu>
2023-01-22 14:56:53 -0500

    Add empty config module

--- bot.py
+++ bot.py
@@ -1,74 +1,80 @@
 import os
+import sys
 from os.path import join, dirname
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-from src.cogs import reactmatch, poll, gif
+from src.cogs import reactmatch, poll, gif, config
 from src.types.constants import (WELCOME_CHANNEL_ID, PRIMARY_GUILD_ID,
                                  WELCOME_MESSAGE, ADMIN_CONSOLE_CHANNEL_ID,
                                  BOT_ONLINE_MESSAGE, BOT_CUSTOM_STATUS,
                                  BOT_COMMAND_PREFIX)
 
-load_dotenv(join(dirname(__file__), '.env'))
 
-TOKEN: str = os.getenv('DISCORD_TOKEN')
+def main():
+    load_dotenv(join(dirname(__file__), '.env'))
 
-intents = discord.Intents.default()
-intents.messages = True
-intents.message_content = True
-intents.typing = True
-intents.presences = True
-intents.members = True
-bot = commands.Bot(
-    command_prefix=BOT_COMMAND_PREFIX,
-    intents=intents
-)
+    token: str = os.getenv('DISCORD_TOKEN')
 
-
-@bot.listen('on_ready')
-async def on_ready(
-
-) -> None:
-    """
-    Event listener triggered when the bot is authenticated and ready to
-    run. Sets up all cogs for the application, notifies the guilds it is
-    present in about its online status, and updates its presence.
-    """
-    print('Initializing HeckBot..')
-    # load cogs
-    await reactmatch.setup(bot)
-    await poll.setup(bot)
-    await gif.setup(bot)
-
-    # alert channels of bot online status
-    for guild in bot.guilds:
-        print(f'{bot.user} has connected to the following guild: '
-              f'{guild.name}(id: {guild.id})')
-        if guild.id == PRIMARY_GUILD_ID:
-            channel = guild.get_channel(ADMIN_CONSOLE_CHANNEL_ID)
-            await channel.send(BOT_ONLINE_MESSAGE)
-    await bot.change_presence(
-        status=discord.Status.online,
-        activity=discord.Game(BOT_CUSTOM_STATUS)
+    intents = discord.Intents.default()
+    intents.messages = True
+    intents.message_content = True
+    intents.typing = True
+    intents.presences = True
+    intents.members = True
+    bot = commands.Bot(
+        command_prefix=BOT_COMMAND_PREFIX,
+        intents=intents
     )
 
+    @bot.listen('on_ready')
+    async def on_ready(
 
-@bot.listen()
-async def on_member_join(
-        member: discord.Member
-) -> None:
-    """
-    Event listener triggered when the bot detects a new member joining
-    the server. Sends a message in whitelisted servers with a designated
-    welcome channel to welcome the user to the server.
-    :param member: Discord member who joined
-    """
-    for guild in bot.guilds:
-        if guild.id == PRIMARY_GUILD_ID:
-            channel = guild.get_channel(WELCOME_CHANNEL_ID)
-            await channel.send(WELCOME_MESSAGE.format(member.id))
+    ) -> None:
+        """
+        Event listener triggered when the bot is authenticated and ready
+        to run. Sets up all cogs for the application, notifies the
+        guilds it is present in about its online status, and updates its
+        presence.
+        """
+        print('Initializing HeckBot..')
+        # load cogs
+        await config.setup(bot)
+        await gif.setup(bot)
+        await poll.setup(bot)
+        await reactmatch.setup(bot)
+
+        # alert channels of bot online status
+        for guild in bot.guilds:
+            print(f'{bot.user} has connected to the following guild: '
+                  f'{guild.name}(id: {guild.id})')
+            if guild.id == PRIMARY_GUILD_ID:
+                channel = guild.get_channel(ADMIN_CONSOLE_CHANNEL_ID)
+                await channel.send(BOT_ONLINE_MESSAGE)
+        await bot.change_presence(
+            status=discord.Status.online,
+            activity=discord.Game(BOT_CUSTOM_STATUS)
+        )
+
+    @bot.listen()
+    async def on_member_join(
+            member: discord.Member
+    ) -> None:
+        """
+        Event listener triggered when the bot detects a new member
+        joining the server. Sends a message in whitelisted servers with
+        a designated welcome channel to welcome the user to the server.
+        :param member: Discord member who joined
+        """
+        for guild in bot.guilds:
+            if guild.id == PRIMARY_GUILD_ID:
+                channel = guild.get_channel(WELCOME_CHANNEL_ID)
+                await channel.send(WELCOME_MESSAGE.format(member.id))
+
+    bot.run(token)
 
 
-bot.run(TOKEN)
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ src/cogs/config.py
@@ -0,0 +1,40 @@
+import os
+
+import aiohttp
+from discord.ext import commands
+from discord.ext.commands import Bot, Context
+
+
+class Config(commands.Cog):
+    """
+    Cog for config-related features in the bot.
+    """
+
+    def __init__(
+            self,
+            bot: Bot
+    ) -> None:
+        """
+        Constructor method
+        :param bot: Instance of the running Bot
+        """
+        self._bot: Bot = bot
+
+    @commands.command()
+    async def gif(
+            self,
+            ctx: Context,
+            config_option: str,
+            config_value: str
+    ) -> None:
+        pass
+
+
+async def setup(
+        bot: Bot
+):
+    """
+    Setup function for registering the gif cog.
+    :param bot: Instance of the running Bot
+    """
+    await bot.add_cog(Config(bot))
