b1f4e9ae9e269d132f70ae171047c0789455d873
Kai H <me@kaihaskell.com>
2023-01-23 00:38:39 -0500

    Added reimagine functionality

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import os
 from os.path import join, dirname
-
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-from src.cogs import reactmatch, poll, gif, sfx, imagegeneration
+from src.cogs import ai, reactmatch, poll, gif, sfx
 from src.types.constants import WELCOME_CHANNEL_ID, PRIMARY_GUILD_ID, WELCOME_MESSAGE, ADMIN_CONSOLE_CHANNEL_ID, \
     BOT_ONLINE_MESSAGE, BOT_CUSTOM_STATUS, BOT_COMMAND_PREFIX
 
@@ -34,7 +33,7 @@ async def on_ready() -> None:
     await poll.setup(bot)
     await gif.setup(bot)
     await sfx.setup(bot)
-    await imagegeneration.setup(bot)
+    await ai.setup(bot)
 
     # alert channels of bot online status
     for guild in bot.guilds:
--- src/cogs/ai.py
+++ src/cogs/ai.py
@@ -1,10 +1,13 @@
-import os
+import os, requests
+from collections import namedtuple
 
 from discord.ext import commands
 from discord.ext.commands import Bot, Context
 from discord.ui import Button
 
 import openai
+
+ImageCacheEntry = namedtuple('ImageCacheEntry', ['user', 'channel','guild'])
 
 class AI(commands.Cog):
 
@@ -13,24 +16,54 @@ class AI(commands.Cog):
         Constructor method
         :param bot: Instance of the running Bot
         """
+        openai.api_key=os.getenv('OPEN_API_KEY')
+
+        self._user_image_cache={}
+
+        self._image_size='1024x1024'
+
         self._bot: Bot = bot
 
     @commands.command()
     async def imagine(self, ctx: Context,*prompt_terms) -> None:
-
-        openai.api_key=os.getenv('OPEN_API_KEY')
 
         prompt= ' '.join(prompt_terms)
 
         artificial_image = openai.Image.create(
             prompt=prompt,
             n=1,
-            size="1024x1024"
+            size=self._image_size
         )
 
         image_url=artificial_image.data[0].url
 
+        self._user_image_cache[ImageCacheEntry(user=ctx.author.id, channel=ctx.channel.id, guild=ctx.guild.id)]=image_url
+
         print(image_url)
+        await ctx.send(image_url)
+
+    @commands.command()
+    async def reimagine(self, ctx: Context,*prompt_terms) -> None:
+
+        image_url=self._user_image_cache.get(ImageCacheEntry(user=ctx.author.id, channel=ctx.channel.id, guild=ctx.guild.id))
+
+        if image_url is None:
+            await ctx.send('There\'s no previous image')
+            return
+
+        response=requests.get(image_url)
+        img = response.content
+
+        artificial_variation = openai.Image.create_variation(
+                image=img,
+                n=1,
+                size=self._image_size
+            )
+
+        image_url=artificial_variation.data[0].url
+
+        self._user_image_cache[ImageCacheEntry(user=ctx.author.id, channel=ctx.channel.id, guild=ctx.guild.id)]=image_url
+
         await ctx.send(image_url)
 
 async def setup(bot: Bot) -> None:
